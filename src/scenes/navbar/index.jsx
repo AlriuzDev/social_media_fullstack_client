73decfe85edecd16f51289b9a1a45bf90593a392
Anderson <avictoral@gmail.com>
2023-10-29 16:15:32 -0400

    Create login and register form pages

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          AlriuzBook
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween

bcc7cf63c3d01e744b295219e5a3842a1591c064
Anderson <avictoral@gmail.com>
2023-10-31 20:25:27 -0400

    updated friend component and create profile page

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -2,8 +2,10 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
-import MyPostWidget from "scenes/widgets/MyPostWidget"
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdWidget from "scenes/widgets/AdWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -29,7 +31,13 @@ const HomePage = () => {
           <MyPostWidget picturePath={picturePath} />
           <PostsWidget userId={_id} />
         </Box>
-        {isNonMobileScreens && <Box flexBasis="26%"> </Box>}
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id}/>
+          </Box>
+        )}
       </Box>
     </Box>
   );
